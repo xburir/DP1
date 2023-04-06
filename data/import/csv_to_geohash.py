4b8528f59e0ce0d96c09bf0d1ed5abe7582f02fb
Maros <cavojsky.maros@gmail.com>
2023-04-06 12:55:36 +0200

    big dataset fix

--- data/import/csv_to_geohash.py
+++ data/import/csv_to_geohash.py
@@ -8,37 +8,64 @@ import sys
 import json
 
 def csvToGeohash(csvPath, dbName, title):
+    print("reading dataset ...")
     df = pd.read_csv(csvPath)
     df = df.fillna('')
+    print("loaded dataset of "+str(len(df))+ " lines")
     #path.csv
-    df['geohash'] = df.apply(
-        lambda row: gh.encode(row['lat'], row['lon'], precision=7),
-        axis=1
-    )
+    lat_col = df.columns.get_loc("lat")
+    lon_col = df.columns.get_loc("lon")
+    track_col = df.columns.get_loc("track")
+    geohash = []
+    dx = pd.DataFrame(geohash, columns=["geohash","track"])
+    dx.to_csv("/home/data/import/files/"+dbName+"_path.csv", index=False, mode="w")
+    row = 0
+    total = 0
+    print("Encoding tracks to geohash sequences into path.csv... ")
+    for track in df.values:
+        geohash.append([gh.encode(track[lat_col], track[lon_col], precision = 7), track[track_col]])
+        row+=1
+        if row>1000000:
+            dx = pd.DataFrame(geohash, columns=["geohash","track"])
+            dx.to_csv("/home/data/import/files/"+dbName+"_path.csv", header=False, index=False, mode="a")
+            row = 0
+            geohash = []
+            total +=1
+            print(str(total)+"M lines done...")
 
-    df_path = df[['geohash', 'track']]
-    df_path.to_csv("/home/data/import/files/"+dbName+"_path.csv", index=False)
-    
+    dx = pd.DataFrame(geohash, columns=["geohash","track"])
+    dx.to_csv("/home/data/import/files/"+dbName+"_path.csv", header=False, index=False, mode="a")
+   
     #track.csv
     tracks = []
-    for id, values in df.groupby('track'):
+    dx = pd.DataFrame(tracks, columns=['route', 'track'])
+    dx.to_csv("/home/data/import/files/"+dbName+"_track.csv", index=False, sep=';', quoting=csv.QUOTE_NONE, mode="w")
+    row = 0
+    total = 0
+    grouped = df.groupby('track')
+    print("Generating geojsons for "+str(len(grouped))+" tracks into track.csv ...")
+    for id, values in grouped:
         tracks.append([id, str(geojson.Feature(geometry=geojson.LineString(values[["lon", "lat"]].values.tolist())))])
-        
-    df_track = pd.DataFrame(tracks, columns=['route', 'track'])
-    df_track.to_csv("/home/data/import/files/"+dbName+"_track.csv", index=False, sep=';', quoting=csv.QUOTE_NONE)
+        row+=1
+        if row>10000:
+            dx = pd.DataFrame(tracks, columns=['route', 'track'])
+            dx.to_csv("/home/data/import/files/"+dbName+"_track.csv", header=False, index=False, sep=';', quoting=csv.QUOTE_NONE, mode="a")
+            row = 0
+            tracks = []
+            total +=1
+            print(str(total*10)+"k tracks done...")
     
+    dx = pd.DataFrame(tracks, columns=['route', 'track'])
+    dx.to_csv("/home/data/import/files/"+dbName+"_track.csv", header=False, index=False, sep=';', quoting=csv.QUOTE_NONE, mode="a")
+    
+    print("Generating db info...")
     #latlon_median.csv
     mapconfig = {"center":{"lat": df["lat"].median(), "lon": df["lon"].median()}, "title": title, "dbname": dbName, "attribution": ""}
     with open("/var/www/html/center/"+dbName+".json", 'w') as outfile:
         outfile.write(json.dumps(mapconfig))
-    # df_lat_mean = df["lat"].median()
-    # df_lon_mean = df["lon"].median()
-    # median_export = []
-    # median_export.append([df_lat_mean, df_lon_mean])
-    # df_median = pd.DataFrame(median_export, columns=['lat', 'lon'])
-    # df_median.to_json("/var/www/html/center/"+dbName+".json")
-
+    
 def importData(dbName):
+    print("creating database...")
     mydb = mysql.connector.connect(
         host="localhost",
         user="search",
@@ -63,7 +90,9 @@ def importData(dbName):
     mycursor.execute("CREATE TABLE `tracks` ( `route` varchar(250) NOT NULL, `track` mediumtext NOT NULL, PRIMARY KEY (`route`)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;")
     
     mycursor.execute("SET GLOBAL local_infile=1;")
+    print("importing tracks ...")
     mycursor.execute("load data local infile '/home/data/import/files/"+dbName+"_path.csv' into table `path` fields terminated by ',' lines terminated by '\n' ignore 1 lines (`hash`, `track`);")
+    print("importing geojsons ...")
     mycursor.execute("load data local infile '/home/data/import/files/"+dbName+"_track.csv' into table `tracks` fields terminated by ';' lines terminated by '\n' ignore 1 lines (`route`, `track`);")
     mydb.commit()
     mycursor.close()
